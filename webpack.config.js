12af73edffddd0471c6a5dfa2b913e92824c0221
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-07-30 18:15:50 +0600

    enable sourcemap for scripts

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const config = {
+    devtool: 'eval',
     entry: {
         main: './src/assets/scripts/main.js'
     },
