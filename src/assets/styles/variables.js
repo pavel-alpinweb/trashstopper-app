3ecc4718f952f6dbef9e77f424c8148fe4a6adeb
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-08-01 13:54:34 +0600

    create variables add conect to font

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,10 +1,14 @@
 module.exports = {
-    "white": "#efeee9",
-    "beige": "#eeebdf",
-    "green": "#16c2aa",
-    "text-color": "#455a64",
-    "grey": "#373e42",
-    "light-grey": "#dfdcd5",
-    "test" : "#000"
-  }
+  "white": "#ffffff",
+  "mainColor": "#163850",
+  "accentColor": "#feb63f",
+  "h1fontSize": "48px",
+  "h2fontSize": "36px",
+  "h3fontSize": "24px",
+  "h4fontSize": "20px",
+  "h5fontSize": "18px",
+  "h6fontSize": "14px",
+  "textFontSize": "16px",
+  "mainFont": "'Montserrat', sans-serif"
+}
   
