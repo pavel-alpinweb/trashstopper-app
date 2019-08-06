8e406b8e5af6f851d5ea0afbf4f3dd04cdc1ec9f
pavel-alpinweb <pavel.alpinweb@yandex.ru>
2019-08-06 19:44:51 +0600

    display images form model in form -- need refact

--- src/assets/scripts/modules/view.js
+++ src/assets/scripts/modules/view.js
@@ -1,3 +1,16 @@
+const form = document.querySelector('[data-window="form-container"]');
+
+const addressEl = document.querySelector('[data-role="addressLine"]');
+const inputEl = document.querySelector('[data-role="place-name"]');
+
+const radioElTrash = document.querySelector('[data-place="trash-place"]');
+const radioElClean = document.querySelector('[data-place="clean-place"]');
+const radioElBox = document.querySelector('[data-place="trashBox-place"]');
+
+const trashGallery = document.querySelector('[data-gallery="trashGallery"]');
+const cleanGallery = document.querySelector('[data-gallery="cleanGallery"]');
+const boxesGallery = document.querySelector('[data-gallery="boxesGallery"]');
+
 const view = {
     createPlacemark(map, coords, textAddress){
         let placemark = new ymaps.Placemark(coords, {
@@ -22,16 +35,12 @@ const view = {
         map.geoObjects.add(cluster);
         return cluster;
     },
-    showForm(data){
-        const form = document.querySelector('[data-window="form-container"]');
-        const addressEl = document.querySelector('[data-role="addressLine"]');
-        const inputEl = document.querySelector('[data-role="place-name"]');
-        const radioElTrash = document.querySelector('[data-place="trash-place"]');
-        const radioElClean = document.querySelector('[data-place="clean-place"]');
-        const radioElBox = document.querySelector('[data-place="trashBox-place"]');
-        
+    showForm(data){       
         addressEl.innerText = data.mapAddress;
         inputEl.value = data.placeName;
+        trashGallery.innerHTML = "";
+        cleanGallery.innerHTML = "";
+        boxesGallery.innerHTML = "";
         if(data.placeType == "trashBox"){
             radioElBox.click();
         } else if(data.placeType == "cleanPlace"){
@@ -39,6 +48,32 @@ const view = {
         } else if(data.placeType == "trashPlace"){
             radioElTrash.click();
         }
+
+        if(data.imageArray.trash.length != 0){
+            for (const src of data.imageArray.trash) {
+                const image = document.createElement('img');
+                image.className='photo-preview';
+                image.src = src;
+                trashGallery.append(image);
+            }  
+        }
+        if(data.imageArray.clean.length != 0){
+            for (const src of data.imageArray.clean) {
+                const image = document.createElement('img');
+                image.className='photo-preview';
+                image.src = src;
+                cleanGallery.append(image);
+            }  
+        }
+        if(data.imageArray.boxes.length != 0){
+            for (const src of data.imageArray.boxes) {
+                const image = document.createElement('img');
+                image.className='photo-preview';
+                image.src = src;
+                boxesGallery.append(image);
+            }  
+        }
+
         form.classList.remove("hide");
     },
     hideForm(){
